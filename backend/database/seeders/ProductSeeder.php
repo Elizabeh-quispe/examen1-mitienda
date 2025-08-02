90412b69b188f87cc5ba3a7757b630abd04b3533
unknown <Elizabeth-quispe>
2025-08-02 19:24:12 -0400

    llenar datos a la tabla producto

--- backend/database/seeders/ProductSeeder.php
+++ backend/database/seeders/ProductSeeder.php
@@ -13,24 +13,6 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('productos')->insert([
-            [
-            'nombres'=>('Hilux'),
-            'precio'=>('50.000'),
-            'marca_id'=>1,
-            'created_at'=> now(),
-            'updated_at'=> now(),
-
-            ],
-            [
-                'nombres'=>('Salomon'),
-                'precio'=>('20.000'),
-                'marca_id'=>2,
-                'created_at'=> now(),
-                'updated_at'=> now(),
-    
-            ]
-        ]
-    );
+       
     }
 }
