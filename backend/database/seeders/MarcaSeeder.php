f326a54b8a96f0a4d227eb223f7bd6ade23f2cc2
unknown <Elizabeth-quispe>
2025-08-02 19:18:07 -0400

    llenar datos a la tabla marca

--- backend/database/seeders/MarcaSeeder.php
+++ backend/database/seeders/MarcaSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MarcaSeeder extends Seeder
 {
@@ -12,6 +13,21 @@ class MarcaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('marcas')->insert([
+            [
+            'nombres'=>('Toyota'),
+            'created_at'=> now(),
+            'updated_at'=> now(),
+
+            ],
+            [
+                'nombres'=>('Suzuki'),
+                'created_at'=> now(),
+                'updated_at'=> now(),
+    
+            ]
+        ]
+    );
+    
     }
 }
--- backend/database/seeders/ProductSeeder.php
+++ backend/database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +13,24 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('productos')->insert([
+            [
+            'nombres'=>('Hilux'),
+            'precio'=>('50.000'),
+            'marca_id'=>1,
+            'created_at'=> now(),
+            'updated_at'=> now(),
+
+            ],
+            [
+                'nombres'=>('Salomon'),
+                'precio'=>('20.000'),
+                'marca_id'=>2,
+                'created_at'=> now(),
+                'updated_at'=> now(),
+    
+            ]
+        ]
+    );
     }
 }
