2e4aab6747a2eb6bd7126884d87a85319e750565
unknown <Elizabeth-quispe>
2025-08-03 10:50:00 -0400

    creacion del controlador para el login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MarcaController;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\AuthController;
 
 Route::apiResource('marcas', MarcaController::class);
 Route::apiResource('productos', ProductoController::class);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
