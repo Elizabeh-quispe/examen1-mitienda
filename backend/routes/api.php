e71316390521e54ad60f5ecf934ece93fdd45c61
unknown <Elizabeth-quispe>
2025-08-03 10:46:12 -0400

    instalacion de sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MarcaController;
-
+use App\Http\Controllers\ProductoController;
 
 Route::apiResource('marcas', MarcaController::class);
+Route::apiResource('productos', ProductoController::class);
